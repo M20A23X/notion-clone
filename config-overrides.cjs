9db8d68e4c3b035d1a584546cf4371b5b038138c
Maksym Shapovalov <maks0681912507@gmail.com>
2024-03-29 23:03:55 -0400

    Fix(config): fix packages, eslint, webpack configs

--- config-overrides.cjs
+++ config-overrides.cjs
@@ -1,4 +1,10 @@
-const {removeModuleScopePlugin, override, addWebpackPlugin} = require("customize-cra");
+const {removeModuleScopePlugin, override, addWebpackPlugin, addWebpackAlias, babelInclude, addWebpackExternals} = require("customize-cra");
+const path = require('path');
 const Dotenv = require("dotenv-webpack");
 
-module.exports = override(removeModuleScopePlugin(), addWebpackPlugin(new Dotenv()),);
+module.exports = override(
+    removeModuleScopePlugin(),
+    addWebpackPlugin(new Dotenv()),
+    addWebpackAlias({["#"]: path.resolve(__dirname, "src")}),
+    babelInclude([path.resolve(path.resolve(__dirname, "src"))])
+);
